17218036f356a15af8b0769273589db026e8e3b3
Your Name <you@example.com>
2020-03-28 12:16:46 -0400

    last

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World_1iidskjfnk");
+        System.out.println( "i1Hello World_1iidskjfnk");
     }
 }
